0333fdb8c8447ee8680d3788214cd28f23d39e46
Tiago de Barros Canadinhas <116236228+Tiago3BC@users.noreply.github.com>
2025-06-04 17:32:13 +0100

    Project Layout

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
 import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Calculador Automático de Horas",
+    page_icon="🧮",
+    layout="wide",
+)
 
 st.title("Calculador Automático de Horas")
+
+st.write(
+    "Esta app permite a execução automática de Horas atrasvés do report de detalhes salariais do Tamigo"
+)
+
+with st.sidebar:
+    uploaded_file = st.file_uploader(
+        "Importa o teu ficheiro excel aqui", type=["xlsx", "xls"]
+    )
+
+    if uploaded_file is None:
+        st.sidebar.warning("Importar um ficheiro do Tamigo", icon="⚠️")
+    else:
+        st.sidebar.success("Dados importados corretamente, Sra. Ângela! ✅")
+        df = pd.read_excel(uploaded_file)
+        st.dataframe(df.head())
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.dataframe(df.head())
